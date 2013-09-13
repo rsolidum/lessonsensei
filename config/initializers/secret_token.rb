96e006ea29758a053b60456c8163bbf4bb35aff2
halolane <myhalolane@gmail.com>
2013-09-12 20:49:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lessonsensei::Application.config.secret_token = 'd03bdce439fd8eda180ccf14ba4fb4277874c0d8b669bd91577625d93dfd8b5b9dab395e935d091df5d5c3c87e60c900be3fee0c3c192f60e0de3259c7a38f50'
